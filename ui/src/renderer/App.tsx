b263283f16d3eaad662d3fc607535f41a7772842
pumm1 <patsu01@gmail.com>
2024-09-08 08:28:30 +0300

    Drop unused ui

--- ui/src/renderer/App.tsx
+++
@@ -1,9 +0,0 @@
-import MediaBrowser from './MediaBrowser'
-import './App.css'
-
-const App = () => 
-<div className='App'>
-  <MediaBrowser />
-</div>
-
-export default App
--- ui/src/renderer/LoadingIndicator.tsx
+++
@@ -1,4 +0,0 @@
-import './LoadingIndicator.css'
-const LoadingIndicator = () => <div className='loadingContainer'><div className='spinner'></div></div>
-
-export default LoadingIndicator
