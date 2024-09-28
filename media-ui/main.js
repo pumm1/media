0c3f7090a949f405426956e5ab651b3b46042f34
pumm1 <patsu01@gmail.com>
2024-09-28 09:53:27 +0300

    Clean UI

--- media-ui/main.js
+++ media-ui/main.js
@@ -20,7 +20,12 @@ function createWindow() {
       contextIsolation: true,   // Recommended for security
       enableRemoteModule: false, // Security measure
       nodeIntegration: false,    // Disable nodeIntegration in renderer
+      zoomFactor: 1.5,
     },
+  });
+
+  mainWindow.webContents.on('did-finish-load', () => {
+    mainWindow.webContents.setZoomFactor(1.5);  // Set zoom to 150%
   });
 
   // Load the React app in dev or production mode
